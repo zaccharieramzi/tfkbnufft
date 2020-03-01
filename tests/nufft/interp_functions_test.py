5efd8b8ea14bb75d941526922d0bc4b633a91b19
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-02 00:15:21 +0100

    refactored interp tests to use the same setup

--- tests/nufft/interp_functions_test.py
+++ tests/nufft/interp_functions_test.py
@@ -8,46 +8,9 @@ from torchkbnufft.nufft.utils import build_table
 from tfkbnufft.nufft import interp_functions as tf_interp_functions
 from ..utils import to_torch_arg, torch_to_numpy, to_tf_arg
 
-
-@pytest.mark.parametrize('conjcoef', [True, False])
-def test_calc_coef_and_indices(conjcoef):
-    grid_size = np.array([800, 800])
-    im_size = grid_size / 2
-    n_samples = 324000
-    # normalized frequency locations (i.e. between -grid_size/2 and grid_size/2)
-    tm = np.random.uniform(-grid_size/2, grid_size/2, size=(n_samples, len(im_size))).T
-    numpoints = np.array([6,] * len(im_size))
-    kofflist = 1 + np.floor(tm - numpoints[:, None] / 2).astype(int)
-    Jval = np.random.uniform(numpoints).astype(int)
-    table_oversamp = 2**10
-    L = np.array([table_oversamp for i in im_size])
-    table = build_table(
-        numpoints=numpoints,
-        table_oversamp=L,
-        grid_size=grid_size,
-        im_size=im_size,
-        ndims=len(im_size),
-        order=(0,) * len(im_size),
-        alpha=tuple(np.array(2.34) * np.array(numpoints)),
-    )
-    numpoints = numpoints.astype('float')
-    L = L.astype('float')
-    grid_size = grid_size.astype('float')
-    im_size = im_size.astype('float')
-    centers = np.floor(numpoints * L / 2).astype(int)
-    args = [tm, kofflist, Jval, table, centers, L, grid_size.astype('int')]
-    torch_args = [to_torch_arg(arg) for arg in args] + [conjcoef]
-    res_torch_coefs, res_torch_ind = torch_interp_functions.calc_coef_and_indices(*torch_args)
-    res_torch_coefs = torch_to_numpy(res_torch_coefs, complex_dim=0)
-    tf_args = [tf.convert_to_tensor(arg) for arg in args] + [conjcoef]
-    res_tf_coefs, res_tf_ind = tf_interp_functions.calc_coef_and_indices(*tf_args)
-    np.testing.assert_equal(res_torch_ind.numpy(), res_tf_ind.numpy())
-    np.testing.assert_allclose(res_torch_coefs, res_tf_coefs.numpy())
-
-def test_run_interp():
+def setup():
     # TODO: test with multicoils, i.e griddat = np.stack([griddat]*n_coils)
     grid_size = np.array([800, 800])
-    griddat = np.reshape(np.random.randn(*grid_size) + 1j * np.random.randn(*grid_size), (1, -1))
     im_size = grid_size / 2
     n_samples = 324000
     # normalized frequency locations (i.e. between -grid_size/2 and grid_size/2)
@@ -68,6 +31,30 @@ def test_run_interp():
     numpoints = numpoints.astype('float')
     L = L.astype('float')
     grid_size = grid_size.astype('float')
+    return tm, Jgen, table, numpoints, L, grid_size
+
+
+@pytest.mark.parametrize('conjcoef', [True, False])
+def test_calc_coef_and_indices(conjcoef):
+    tm, Jgen, table, numpoints, L, grid_size = setup()
+    kofflist = 1 + np.floor(tm - numpoints[:, None] / 2).astype(int)
+    Jval = Jgen[0]
+    centers = np.floor(numpoints * L / 2).astype(int)
+    args = [tm, kofflist, Jval, table, centers, L, grid_size.astype('int')]
+    torch_args = [to_torch_arg(arg) for arg in args] + [conjcoef]
+    res_torch_coefs, res_torch_ind = torch_interp_functions.calc_coef_and_indices(*torch_args)
+    res_torch_coefs = torch_to_numpy(res_torch_coefs, complex_dim=0)
+    tf_args = [tf.convert_to_tensor(arg) for arg in args] + [conjcoef]
+    res_tf_coefs, res_tf_ind = tf_interp_functions.calc_coef_and_indices(*tf_args)
+    np.testing.assert_equal(res_torch_ind.numpy(), res_tf_ind.numpy())
+    np.testing.assert_allclose(res_torch_coefs, res_tf_coefs.numpy())
+
+def test_run_interp():
+    tm, Jgen, table, numpoints, L, grid_size = setup()
+    griddat = np.reshape(
+        np.random.randn(*grid_size.astype('int')) + 1j * np.random.randn(*grid_size.astype('int')),
+        (1, -1),  # 1 coil
+    )
     params = {
         'dims': grid_size.astype('int'),
         'table': table,

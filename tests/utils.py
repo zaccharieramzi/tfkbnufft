0c934cec953ac6d09456e3d324f1a9e4b713a74e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-02 00:09:10 +0100

    support for dicts in numpy to dl framework util

--- tests/utils.py
+++ tests/utils.py
@@ -1,16 +1,25 @@
 import numpy as np
+import tensorflow as tf
 import torch
 
 
-def to_torch_arg(array):
-    if isinstance(array, list):
-        return [to_torch_arg(a) for a in array]
+def to_torch_arg(arg):
+    if isinstance(arg, list):
+        return [to_torch_arg(a) for a in arg]
+    elif isinstance(arg, dict):
+        return {k: to_torch_arg(v) for k, v  in arg.items()}
     else:
-        if np.iscomplex(array).any():
-            torch_x = np.stack((np.real(array), np.imag(array)))
+        if np.iscomplex(arg).any():
+            torch_x = np.stack((np.real(arg), np.imag(arg)))
             return torch.tensor(torch_x)
         else:
-            return torch.tensor(array)
+            return torch.tensor(arg)
+
+def to_tf_arg(arg):
+    if isinstance(arg, dict):
+        return {k: to_tf_arg(v) for k, v in arg.items()}
+    else:
+        return tf.convert_to_tensor(arg)
 
 def torch_to_numpy(array, complex_dim=None):
     if complex_dim is not None:

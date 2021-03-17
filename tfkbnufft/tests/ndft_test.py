5a78ff3a3e1c98fc9691b161dcd62f1df52ffe65
chaithyagr <chaithyagr@gitlab.com>
2021-03-17 15:47:24 +0100

    Update testing

--- tfkbnufft/tests/ndft_test.py
+++ tfkbnufft/tests/ndft_test.py
@@ -5,12 +5,22 @@ from tfkbnufft import kbnufft_forward, kbnufft_adjoint
 from tfkbnufft.kbnufft import KbNufftModule
 
 
-@pytest.mark.parametrize('im_size', [(20, ), (10, 10), (10, 10, 10)])
+def get_fourier_matrix(ktraj, grid_r, im_size, im_rank, do_ifft=False):
+    traj_grid = tf.cast(tf.matmul(tf.transpose(ktraj[0]), grid_r), tf.complex64)
+    if do_ifft:
+        A = tf.exp(1j * traj_grid)
+    else:
+        A = tf.exp(-1j * traj_grid)
+    A = A / (np.sqrt(tf.reduce_prod(im_size)) * np.power(np.sqrt(2), im_rank))
+    return A
+
+
+@pytest.mark.parametrize('im_size', [(10, ), (10, 10)])
 def test_adjoint_and_gradients(im_size):
     tf.random.set_seed(0)
     grid_size = tuple(np.array(im_size)*2)
     im_rank = len(im_size)
-    M = im_size[0] * 3**im_rank
+    M = im_size[0] * 2**im_rank
     nufft_ob = KbNufftModule(im_size=im_size, grid_size=grid_size, norm='ortho', grad_traj=True)
     # Generate Trajectory
     ktraj = tf.Variable(tf.random.uniform((1, im_rank, M), minval=-1/2, maxval=1/2)*2*np.pi)
@@ -22,9 +32,7 @@ def test_adjoint_and_gradients(im_size):
         I_nufft = kbnufft_adjoint(nufft_ob._extract_nufft_interpob())(kdata, ktraj)[0][0]
         r = [tf.linspace(-im_size[i]/2, im_size[i]/2-1, im_size[i]) for i in range(im_rank)]
         grid_r =tf.cast(tf.reshape(tf.meshgrid(*r ,indexing='ij'), (im_rank, tf.reduce_prod(im_size))), tf.float32)
-        A = tf.exp(1j * tf.cast(tf.matmul(tf.transpose(ktraj[0]), grid_r), tf.complex64)) / (
-            np.sqrt(tf.reduce_prod(im_size)) * np.power(np.sqrt(2), im_rank)
-        )
+        A = get_fourier_matrix(ktraj, grid_r, im_size, im_rank, do_ifft=True)
         I_ndft = tf.reshape(tf.matmul(tf.transpose(A), kdata[0][0][..., None]), im_size)
 
     tf_test = tf.test.TestCase()
@@ -44,12 +52,12 @@ def test_adjoint_and_gradients(im_size):
     # gradient_from_matrix = 2*np.pi*1j*tf.matmul(tf.cast(r, tf.complex64), tf.transpose(A))*kdata[0][0]
 
 
-@pytest.mark.parametrize('im_size', [(20, ), (10, 10)])
+@pytest.mark.parametrize('im_size', [(10, ), (10, 10)])
 def test_forward_and_gradients(im_size):
     tf.random.set_seed(0)
     grid_size = tuple(np.array(im_size)*2)
     im_rank = len(im_size)
-    M = im_size[0] * 3**im_rank
+    M = im_size[0] * 2**im_rank
     nufft_ob = KbNufftModule(im_size=im_size, grid_size=grid_size, norm='ortho', grad_traj=True)
     # Generate Trajectory
     ktraj = tf.Variable(tf.random.uniform((1, im_rank, M), minval=-1/2, maxval=1/2)*2*np.pi)
@@ -60,9 +68,7 @@ def test_forward_and_gradients(im_size):
         kdata_nufft = kbnufft_forward(nufft_ob._extract_nufft_interpob())(signal, ktraj)[0]
         r = [tf.linspace(-im_size[i]/2, im_size[i]/2-1, im_size[i]) for i in range(im_rank)]
         grid_r =tf.cast(tf.reshape(tf.meshgrid(*r ,indexing='ij'), (im_rank, tf.reduce_prod(im_size))), tf.float32)
-        A = tf.exp(-1j * tf.cast(tf.matmul(tf.transpose(ktraj[0]), grid_r), tf.complex64)) / (
-            np.sqrt(tf.reduce_prod(im_size)) * np.power(np.sqrt(2), im_rank)
-        )
+        A = get_fourier_matrix(ktraj, grid_r, im_size, im_rank, do_ifft=False)
         kdata_ndft = tf.transpose(tf.matmul(A, tf.reshape(signal[0][0], (tf.reduce_prod(im_size), 1))))
 
     tf_test = tf.test.TestCase()

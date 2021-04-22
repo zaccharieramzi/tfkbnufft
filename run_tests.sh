88611431868f328697e88eb9c5fcdc8463811422
chaithyagr <chaithyagr@gitlab.com>
2021-04-22 11:40:03 +0200

    Set -x

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 pip install torch==1.7 torchkbnufft==0.3.4 scikit-image pytest
 # We test ndft_test.py separately as it causes some issues with tracing resulting in hangs.
 python -m pytest tfkbnufft --ignore=tfkbnufft/tests/ndft_test.py
